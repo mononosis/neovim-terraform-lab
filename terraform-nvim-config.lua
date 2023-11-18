df371d1c6b9fef37c14a86117a7cd5877449e225
Your Name <you@example.com>
2023-11-18 15:44:23 +0000

    add terraform config

---
+++ terraform-nvim-config.lua
@@ -0,0 +1,4 @@
+vim.cmd('set background=dark')  -- Set background to dark
+vim.cmd('colorscheme blue')     -- Use a colorscheme that emphasizes blue tones
+-- Add any other Terraform-specific configurations here
+
