be23a26b7a0dce0e6d948573dabb1c1db3703439
Your Name <you@example.com>
2023-11-19 07:52:31 +0000

    test

--- terraform-nvim-config.lua
+++ terraform-nvim-config.lua
@@ -19,3 +19,25 @@ vim.cmd('set background=dark')  -- Set background to dark
 vim.cmd('colorscheme blue')     -- Use a colorscheme that emphasizes blue tones
 -- Add any other Terraform-specific configurations here
 
+-- Function to split a string by a delimiter
+local function split(str, delimiter)
+    local result = {}
+    for match in (str..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match)
+    end
+    return result
+end
+
+-- Function to add directories to the runtimepath
+local function add_paths_to_rtp(paths)
+    for _, path in ipairs(paths) do
+        vim.o.runtimepath = vim.o.runtimepath .. ',' .. path
+    end
+end
+
+-- Use environment variable to get the concatenated paths
+local plugin_paths = os.getenv('NVIM_PLUGIN_PATHS')
+if plugin_paths then
+    local paths = split(plugin_paths, ':')
+    add_paths_to_rtp(paths)
+end
