6e66418094ce2d294a312543f38319c04997f3b7
Your Name <you@example.com>
2023-11-20 07:36:58 +0000

    change colour

--- terraform-nvim-config.lua
+++ terraform-nvim-config.lua
@@ -1,43 +1,34 @@
--- Packer Bootstrap
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
-  vim.cmd 'packadd packer.nvim'
-end
-
--- Packer Configuration
-require('packer').startup(function(use)
-    use 'hashivim/vim-terraform'
-
-    -- Add more plugins as needed
-end)
-
-vim.cmd([[autocmd VimEnter * PackerInstall]])
-
-
 vim.cmd('set background=dark')  -- Set background to dark
-vim.cmd('colorscheme blue')     -- Use a colorscheme that emphasizes blue tones
+vim.cmd('colorscheme morning')     -- Use a colorscheme that emphasizes blue tones
 -- Add any other Terraform-specific configurations here
 
--- Function to split a string by a delimiter
-local function split(str, delimiter)
-    local result = {}
-    for match in (str..delimiter):gmatch("(.-)"..delimiter) do
-        table.insert(result, match)
-    end
-    return result
-end
+local lspconfig = require('lspconfig')
 
--- Function to add directories to the runtimepath
-local function add_paths_to_rtp(paths)
-    for _, path in ipairs(paths) do
-        vim.o.runtimepath = vim.o.runtimepath .. ',' .. path
-    end
-end
+lspconfig.terraformls.setup{
 
--- Use environment variable to get the concatenated paths
-local plugin_paths = os.getenv('NVIM_PLUGIN_PATHS')
-if plugin_paths then
-    local paths = split(plugin_paths, ':')
-    add_paths_to_rtp(paths)
-end
+    on_attach = function(_, bufnr)
+    -- Custom on_attach logic
+       vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
+    end,
+
+    flags = {
+        debounce_text_changes = 150,
+    },
+    settings = {
+        -- Language server specific settings
+    },
+    handlers = {
+        -- Custom handlers for language server events
+    },
+}
+
+-- Tflint setup remains the same
+lspconfig.tflint.setup {
+    cmd = { "tflint", "--langserver" },
+    filetypes = { "terraform" },
+    root_dir = lspconfig.util.root_pattern(".terraform", "*.tf"),
+    flags = {
+        debounce_text_changes = 150,
+    }
+}
+
